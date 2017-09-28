9984dee24ccfb9d76f8efc5ac5244b407b85dd7c
mekthejacker <shae.roberts@gmail.com>
2017-09-28 03:59:22 +0000

    cleaned up some syntax issues, still having problem with did. join maybe?

--- Sites.sql
+++ Sites.sql
@@ -1,12 +1,11 @@
-use Errwater
+use Errwater;
 SHOW TABLES;
 CREATE TABLE Sites (
 sid TINYINT NOT NULL AUTO_INCREMENT,
-sname varchar(19)
+sname varchar(19),
 location varchar(1),
 security varchar(1),
-security varchar(1),
-size varchar(1),
+space varchar(1),
 PRIMARY KEY (sid),
 index(did),
 FOREIGN KEY(did) REFERENCES Districts(did) ON UPDATE CASCADE ON DELETE CASCADE );
